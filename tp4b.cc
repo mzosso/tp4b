7a8a75bd45b0f6ba463264d8a8b22fb626153e14
mzosso <mia.zosso@epfl.ch>
2023-03-13 18:24:03 +0100

    ¨signals¨

--- tp4b.cc
+++ tp4b.cc
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
-
+#include <typeinfo>
 using namespace std;
 
 class Simple
@@ -34,12 +34,14 @@ class Temperature: public Simple {
 	
 int n=1000; //how many pts for d and E
 double n_d=300; //thickness
+double dx=(double) n_d/n; //dx of E and d
 //int V=300; //external
 int s=100; //how many ns
 int n_t=1000; //how many pts for s and I
+double dt= (double) s/n_t;
 int V_d=100; //depletion voltage
 
-vector<double> tps_e(double ini, double n_d, double mu_e,double mu_h, float E[n]) //ini c'est où on injecte
+vector<double> tps(double ini, double n_d, double mu_e,double mu_h, float E[n]) //ini c'est où on injecte // drift time for h and e
 {
 	vector<double> t(2);
 	double dist=n_d-ini; //distance à parcourir pour l'électron
@@ -93,6 +95,51 @@ void fct_E(float E[n], float d[n],int V_d, int V, bool cst=0) //pour pouvoir dé
 			}    
 	}
 }
+
+void fct_I(float I[n], float t[n],double dt,double tps, bool cst=0) 
+{
+	tps*=1e9;
+	for(int i(0); i<n_t;++i)
+		{
+			//t.push_back(s/n_t*i);
+			t[i] = (double) s/n_t*i;
+		}
+		
+	if (cst==0)
+	{
+		TRandom *rand = new TRandom();
+		
+		
+		double r = 0;
+		for(int i(0); i<n_t;++i)
+		{
+			//I.push_back(0);
+			r = rand->Gaus(0,1);
+			I[i]=r;
+		}
+	}
+	else
+	{
+		cout<<"tps"<<tps<<endl;
+		cout<<"dt"<<dt<<endl;
+		
+		int num_dt=round(tps/dt); //nombre de dt
+		cout<<"num_dt"<<num_dt<<endl;
+		double height_I=(double) 1/tps; // aire du rectangle =1
+		cout<<height_I<<endl;
+		for(int j(0); j<num_dt; ++j)
+		{
+			I[j]=height_I;
+			
+		}
+		for(int g(num_dt); g<n_t;++g)
+		{
+			I[g]=0;
+		}
+		
+	}
+}
+
 void tp4b(int V=300)
 {
 	
@@ -103,31 +150,19 @@ void tp4b(int V=300)
 	float E[n];
 	float d[n];
 	float t[n_t];
-	float I[n_t];
+	float I_e[n_t];
+	float I_h[n_t];
 	
-	TRandom *rand = new TRandom();
 	
-	for(int i(0); i<n_t;++i)
-	{
-		//t.push_back(s/n_t*i);
-		t[i] = (double) s/n_t*i;
-	}
-	
-	double r = 0;
-	for(int i(0); i<n_t;++i)
-	{
-		//I.push_back(0);
-		r = rand->Gaus(0,1);
-		I[i]=r;
-	}
 	
 	fct_E(E, d, V_d,V, 1);
-	//fct_E(E, d, V_d,10, 1);
-	//fct_E(E, d, V_d,-10, 1);
+	
 	double ini=200;
 	double mu_e=1350/pow(10,-4);
 	double mu_h=450/pow(10,-4);
-	vector<double> temps=tps_e(ini, n_d, mu_e, mu_h, E);
+	vector<double> temps=tps(ini, n_d, mu_e, mu_h, E);
+	fct_I(I_e, t, dt, temps[0], 1);
+	fct_I(I_h, t, dt, temps[1], 1);
 	
 	while (gPad !=0) gPad ->Close();
 	
@@ -139,32 +174,16 @@ void tp4b(int V=300)
 	TGaxis::SetMaxDigits(4);
 	
 	gStyle->SetOptStat(00000000);
-	//float TA[5]={14.2 , 50, 75, 100, 150};
-	//float VBDA[5]={86.58, 86.71, 87.35, 88.13, 90.45};
-		
-	//TCanvas *canv0 = new TCanvas("canv0", "Sr-90", 200, 10, 1000, 650);
+	
 	TCanvas *canv0 = new TCanvas("canv0", "E", 200, 10, 1000, 650);
 	canv0->SetGrid();
 	
-
-	//TH2F *hpx0 = new TH2F("hpx0", "HPK", 20, 0, 450, 100, 70, 116);
 	TH2F *hpx0 = new TH2F("hpx0", "Electric", 20, 0, 300, 100, 0, 1e3);
 	
 	hpx0->Draw();
 	
-	//hpx0->GetYaxis()->SetTitle("Breakdown, V");
 	hpx0->GetYaxis()->SetTitle("E");
 	
-	
-	/*hpx0->GetYaxis()->SetLabelSize(0.05);
-	hpx0->GetXaxis()->SetLabelSize(0.05);
-	hpx0->GetYaxis()->SetTitleSize(0.05);
-	hpx0->GetXaxis()->SetTitleSize(0.05);
-	hpx0->GetXaxis()->SetNoExponent();
-	hpx0->SetTitle("SR-90, GAIN vs TEMPERATURE");
-	hpx0->GetYaxis()->CenterTitle();
-	hpx0->GetXaxis()->SetTitle("Temperature, K");
-	hpx0->GetXaxis()->CenterTitle();*/
 	
 	hpx0->GetYaxis()->SetLabelSize(0.05);
 	hpx0->GetXaxis()->SetLabelSize(0.05);
@@ -176,16 +195,6 @@ void tp4b(int V=300)
 	hpx0->GetXaxis()->SetTitle("d");
 	hpx0->GetXaxis()->CenterTitle();
 	
-	
-	/*TGraph *gr42 = new TGraph (5, TA, VBDA);
-	gr42 ->SetMarkerStyle(20);
-	gr42 ->SetMarkerColor(2);
-	gr42 ->SetMarkerSize(1.2);
-	gr42 ->SetLineWidth(2);
-	gr42 ->SetLineColor(2);
-	
-	gr42->Draw("LP");*/
-	
 	TGraph *gr_E = new TGraph (n, d, E);
 	gr_E ->SetMarkerStyle(20);
 	gr_E ->SetMarkerColor(4);
@@ -195,12 +204,20 @@ void tp4b(int V=300)
 	
 	gr_E->Draw("LP");
 	
+	
+	//Section different signals on same figure
+	
 	TCanvas *canv1 = new TCanvas("canv1", "I", 200, 10, 1000, 650);
 	canv1->SetGrid();
 	
-	TH2F *hpx1 = new TH2F("hpx1", "I", 20, 0, s, 100, -20, 100);
+	TMultiGraph *mg = new TMultiGraph();
+	mg->SetTitle("Signals");
+
 	
-	hpx1->Draw();
+	
+	/*TH2F *hpx1 = new TH2F("hpx1", "I", 20, 0, s, 100, -1, 1);
+	hpx1->Draw("apl");
+	
 	
 	hpx1->GetYaxis()->SetTitle("I");
 
@@ -212,19 +229,28 @@ void tp4b(int V=300)
 	hpx1->SetTitle("I, ns vs I");
 	hpx1->GetYaxis()->CenterTitle();
 	hpx1->GetXaxis()->SetTitle("ns");
-	hpx1->GetXaxis()->CenterTitle();
+	hpx1->GetXaxis()->CenterTitle();*/
 	
 	
 	
-	TGraph *gr_I = new TGraph (n_t, t, I);
-	gr_I ->SetMarkerStyle(20);
-	gr_I ->SetMarkerColor(2);
-	gr_I ->SetMarkerSize(1.0);
-	gr_I ->SetLineWidth(2);
-	gr_I ->SetLineColor(2);
+	TGraph *gr_I_e = new TGraph (n_t, t, I_e);
+	gr_I_e ->SetMarkerStyle(20);
+	gr_I_e ->SetMarkerColor(2);
+	gr_I_e ->SetMarkerSize(1.0);
+	gr_I_e ->SetLineWidth(2);
+	gr_I_e ->SetLineColor(2);
 	
-	gr_I->Draw("LP");
 	
+	TGraph *gr_I_h = new TGraph (n_t, t, I_h);
+	gr_I_h ->SetMarkerStyle(20);
+	gr_I_h ->SetMarkerColor(2);
+	gr_I_h ->SetMarkerSize(1.0);
+	gr_I_h ->SetLineWidth(2);
+	gr_I_h ->SetLineColor(5);
+	
+	mg->Add(gr_I_h);
+	mg->Add(gr_I_e);	
+	mg->Draw("AC");
 	
 	
 	
