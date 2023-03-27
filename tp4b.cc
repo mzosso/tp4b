2f7a5322f6df812e5d407f2d110768602e9b3bad
mzosso <mia.zosso@epfl.ch>
2023-03-27 19:54:20 +0200

    unidades

--- tp4b.cc
+++ tp4b.cc
@@ -9,15 +9,15 @@ using namespace std;
 
 	
 const int n=1000; //how many pts for d and E
-double n_d=300; //thickness
-double dx=(double) n_d/n; //dx of E and d
+double n_d=300; //thickness, micro
+double dx=(double) n_d/n; //dx of E and d //micro
 //int V=300; //external
 int s=100; //how many ns
 int n_t=1000; //how many pts for s and I
-double dt= (double) s/n_t;
+double dt= (double) s/n_t; //ns
 int V_d=100; //depletion voltage
 int n_e=10; //nb electrons
-double T=298.16;
+double T=298.16; //K
 
 /*const vector<vector<Float_t>> vel(double n_d, float E[n], double T)
 {
@@ -52,24 +52,24 @@ Float_t* vel_e( float E_[n], double T)
 		v_e[i]=(1.42*pow(10,9)*pow(T, -2.42)*E_[i])/pow((pow((1+(E_[i]/1.01)*pow(T,1.55)),2.57*pow(10,-2))*pow(T, 0.66)),(1/(2.57*pow(10,-2)*pow(T,0.66))));
 	}
 	
-	return v_e;
+	return v_e; //cm/s
 }
 
-Float_t* vel_h(double n_d, float v_h[n], float E[n], double T)
+/*Float_t* vel_h(double n_d, float v_h[n], float E[n], double T)
 {
 	vector<double> dist(n);
 	vector<double> ini(n);
 	vector<int> i_h(n);
 	for(int i(0); i<n; ++i)
 	{
-		ini[i]=(double) n_d/n_e*i+n_d/n_e*0.5; //a corregir
-		dist[i]=n_d-ini[i]; //distance à parcourir pour l'électron
+		ini[i]=(double) n_d/n_e*i+n_d/n_e*0.5; //micro
+		dist[i]=n_d-ini[i]; //distance à parcourir pour l'électron, micro
 		i_h[i]=round(n*ini[i]/n_d); // ATTENTION, ROUND DONC PAS E EXACT
 		v_h[i]=(1.31*pow(10,8)*pow(T, -2.2)*E[i_h[i]])/pow((pow((1+(E[i_h[i]]/1.24)*pow(T,1.68)),0.46)*pow(T, 0.17)),(1/0.46*pow(T,0.17)));
 	}
 	
 	return v_h;
-}
+}*/
 
 vector<vector<double>> tps(float ini[n_e], double n_d,float E[n], double T) //ini c'est où on injecte // drift time for h and e
 {
@@ -85,24 +85,24 @@ vector<vector<double>> tps(float ini[n_e], double n_d,float E[n], double T) //in
 	
 	for(int i(0); i<n_e; ++i)
 	{
-		ini[i]=(double) n_d/n_e*i+n_d/n_e*0.5; //a corregir
-		dist[i]=n_d-ini[i]; //distance à parcourir pour l'électron
+		ini[i]=(double) n_d/n_e*i+n_d/n_e*0.5; // micrometre
+		dist[i]=n_d-ini[i]; //distance à parcourir pour l'électron, micrometre
 		i_e[i]=round(n*dist[i]/n_d); // avec d[i]=dist=n_d/n*i
 		//cout<<"hello"<<i<<endl;
 		//cout<<"dist"<<dist[i]<<endl;
 		//cout<<"i_e"<<i_e[i]<<endl;
 		i_h[i]=round(n*ini[i]/n_d); // ATTENTION, ROUND DONC PAS E EXACT
-		v_e[i]=(1.42*pow(10,9)*pow(T, -2.42)*E[i_e[i]])/pow((pow((1+(E[i_e[i]]/1.01)*pow(T,1.55)),2.57*pow(10,-2))*pow(T, 0.66)),(1/2.57*pow(10,-2)*pow(T,0.66)));
+		v_e[i]=(1.42*pow(10,9)*pow(T, -2.42)*E[i_e[i]])/pow((pow((1+(E[i_e[i]]/1.01)*pow(T,1.55)),2.57*pow(10,-2))*pow(T, 0.66)),(1/2.57*pow(10,-2)*pow(T,0.66))); //cm /s
 		//cout<<"hola"<<i<<endl;
 		//cout<<"ini"<<ini[i]<<endl;
-		v_h[i]=(1.31*pow(10,8)*pow(T, -2.2)*E[i_h[i]])/pow((pow((1+(E[i_h[i]]/1.24)*pow(T,1.68)),0.46)*pow(T, 0.17)),(1/0.46*pow(T,0.17)));
+		v_h[i]=(1.31*pow(10,8)*pow(T, -2.2)*E[i_h[i]])/pow((pow((1+(E[i_h[i]]/1.24)*pow(T,1.68)),0.46)*pow(T, 0.17)),(1/0.46*pow(T,0.17))); //cm/s
 		//v_e[i]=mu_e*E[i_e[i]];
 		//v_h[i]=mu_h*E[i_h[i]];
 		//t_e[i]=dist[i]/v_e[i];
 		//t_h[i]=ini[i]/v_h[i];
 		
-		t[0][i]=dist[i]/v_e[i];
-		t[1][i]=ini[i]/v_h[i];
+		t[0][i]=dist[i]*1e4/v_e[i]; //micrometre*1e4/(cm/s)=s
+		t[1][i]=ini[i]*1e4/v_h[i];
 	}
 	cout<<dist[0];
 	cout<<ini[0];
@@ -124,23 +124,23 @@ void fct_E(float E[n], float d[n],int V_d, int V, bool cst=0) //pour pouvoir dé
 		for(int i(0); i<n;++i)
 		{
 			if (cst==0)
-				{E[i]=V;}
+				{E[i]=V/n_d;}
 			else
 			{
 				/*d.push_back(n_d/n*(n-i));
 				E.push_back(V/d[i]);*/
 				//d[i]= (double) n_d/n*(n-i);
-				d[i] = (double) n_d/n*i;
+				d[i] = (double) n_d/n*i; //micrometre
 				if (V_d<=V)
 				{
-					double dif=(V-V_d);
-					E[i]=(double) V_d+dif+a*d[i];
+					double dif=(V-V_d)/n_d;//volt/micrometre
+					E[i]=(double) 1e4*(V_d/n_d+dif+a*d[i]/n_d);// volt/micrometre*1e4=volt/cm
 					
 				}
 				else
 				{
-					double dif=(V-V_d)/n_d;
-					E[i]=(double) (dif+2.0/n_d*V_d*(1-d[i]/double(n_d)));
+					double dif=(V-V_d)/n_d;// volt/micrometre
+					E[i]=(double) 1e4*(dif+2.0/n_d*V_d*(1-d[i]/double(n_d)));//I transform it to volt/cm
 				}
 				/*for (in j(0); j<=nb;++j)
 				{
@@ -158,7 +158,7 @@ void fct_I(float I[n_t], float t[n_t],double dt,vector<double> tps, bool cst=0)
 	for(int i(0); i<n_t;++i)
 		{
 			//t.push_back(s/n_t*i);
-			t[i] = (double) s/n_t*i;
+			t[i] = (double)1e9* s/n_t*i;//ns*1e9=s
 		}
 		
 	if (cst==0)
@@ -185,16 +185,16 @@ void fct_I(float I[n_t], float t[n_t],double dt,vector<double> tps, bool cst=0)
 				{
 					//cout<<"i"<<i;
 					//tps[i]*=1e9;
-					num_dt[i]=round(tps[i]/dt);//nombre de dt
+					num_dt[i]=round(tps[i]/(dt*1e9));//nombre de dt
 					
 					cout<<i<<endl<<"temps  "<<tps[i]<<endl;
 					//cout<<"dt  "<<dt<<endl;
-					height_I[i]=(double) 1/tps[i]; // aire du rectangle =1
+					height_I[i]=(double) 1.0/tps[i]; // aire du rectangle =1
 					cout<<"height   "<<height_I[i]<<endl;
 			
 					for(int j(0); j<num_dt[i]; ++j)
 					{
-						cout<<"gutenTAg"<<j<<endl;
+						//cout<<"gutenTAg"<<j<<endl;
 						I[j]+=height_I[i];
 						//cout<<"size I_nv_ligne"<<I_nv[i].size()<<endl<<"size I_nv colonne"<<I_nv.size()<<j<<endl;		
 					}
@@ -221,7 +221,7 @@ void tp4b(int V=300)
 	{
 		E_[l]=0;
 	}
-    const double start = 3.0;
+    const double start = 2.0;
     const double end = 5.0;
     const double step = (end - start) / (N - 1);
     for (int i = 0; i < N; i++) {
@@ -380,7 +380,7 @@ void tp4b(int V=300)
 		cout<<"ve "<<j<< ":  "<<ve[j]<<endl;
 	}
 	TCanvas *canvas2 = new TCanvas("canvas", "Electron velocity vs E", 200, 10, 1000, 650);
-    TH2F *hpx2 = new TH2F("hpx2", "Electric", 20, 0, n, 3e5, 3e6);
+    TH2F *hpx2 = new TH2F("hpx2", "Electric", 100, 1e5,n, 1e6, 1e5, 1e6);
 	
 	hpx2->Draw();
 	hpx2->GetYaxis()->SetTitle("vel");
